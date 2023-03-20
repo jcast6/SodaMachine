8fed82b500e7e7ec54adb5ded05d873b5cc72d29
jcast6 <89822103+jcast6@users.noreply.github.com>
2023-03-19 17:31:55 -0700

    I added all the code for the soda machine to work.

--- src/comSodaMachine/SodaPopMachine.java
+++ src/comSodaMachine/SodaPopMachine.java
@@ -1,4 +1,42 @@
 package comSodaMachine;
 
 public class SodaPopMachine {
+    private int numCans;
+    private double money;
+
+    public SodaPopMachine(int numCans, double money) {
+        this.numCans = numCans;
+        this.money = money;
+    }
+
+    public void buySoda() {
+        if (numCans > 0) {
+            System.out.println("Enjoy your soda!");
+            numCans--;
+            money += 1.5;
+        } else {
+            System.out.println("Sorry, no more soda cans available.");
+        }
+    }
+
+    public void refill(int numCansToAdd) {
+        numCans += numCansToAdd;
+        System.out.println("Machine refilled with " + numCansToAdd + " soda cans.");
+    }
+
+    public void displayStatus() {
+        System.out.println("Number of soda cans: " + numCans);
+        System.out.println("Amount of money in machine: $" + money);
+    }
+
+    public static void main(String[] args) {
+        SodaPopMachine machine = new SodaPopMachine(10, 15.0);
+
+        machine.displayStatus();
+        machine.buySoda();
+        machine.buySoda();
+        machine.refill(5);
+        machine.buySoda();
+        machine.displayStatus();
+    }
 }
