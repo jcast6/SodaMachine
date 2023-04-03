0de88983dcebedc77f755220ec3a1b0f4413d30c
jcast6 <89822103+jcast6@users.noreply.github.com>
2023-04-03 12:02:52 -0700

    Formatted code.

--- src/fundamentals/Animal.java
+++ src/fundamentals/Animal.java
@@ -1,4 +1,47 @@
 package fundamentals;
 
-public class Animal {
+// Superclass
+class Animal {
+    String name;
+
+    Animal(String name) {
+        this.name = name;
+    }
+
+    void makeSound() {
+        System.out.println("Animal making sound...");
+    }
+}
+
+// Subclass
+class Dog extends Animal {
+    Dog(String name) {
+        super(name);
+    }
+
+    // Override superclass method
+    void makeSound() {
+        System.out.println("Woof!");
+    }
+
+    void wagTail() {
+        System.out.println(name + " is wagging its tail...");
+    }
+
+    public static void main(String[] args) {
+        // Create Animal object
+        Animal animal = new Animal("Generic animal");
+
+        // Animal making sound...
+        animal.makeSound();
+
+        // Create Dog object
+        Dog dog = new Dog("Fido");
+
+        // Woof!
+        dog.makeSound();
+
+        // Fido is wagging its tail...
+        dog.wagTail();
+    }
 }
