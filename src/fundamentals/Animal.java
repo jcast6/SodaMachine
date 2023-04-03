242dd71f8c39a3465876a5697e1ca714995b0f7c
jcast6 <89822103+jcast6@users.noreply.github.com>
2023-04-03 12:07:07 -0700

    Added comments explaining abstraction in the code.

--- src/fundamentals/Animal.java
+++ src/fundamentals/Animal.java
@@ -1,5 +1,27 @@
 package fundamentals;
 
+/**
+ * Inheritance is a mechanism in object-oriented programming
+ * that allows a class to inherit properties and behaviors from another class.
+ *
+ * the Animal class has an abstract method named makeSound().
+ * This means that any subclass of Animal must implement its own version of the
+ * makeSound() method, and the implementation of the method will be specific to
+ * that subclass.
+ *
+ * Abstraction is the process of hiding implementation details and providing
+ * a simplified interface for the user. In this example, the Animal class is an abstraction
+ * of an animal in general, and we provide a method makeSound() that any subclass must implement.
+ * This means that we don't have to worry about the specific details of each type of animal when we're dealing with the
+ * Animal class; we can simply call makeSound() and the appropriate sound will be produced,
+ * regardless of the specific type of animal.
+ *
+ * In addition, the Animal class has a constructor that sets the name variable.
+ * This is another example of abstraction, as it allows us to create instances of the Animal class with a
+ * specific name, without having to worry about the implementation details of how the name
+ * is stored or retrieved.
+ */
+
 // Superclass
 class Animal {
     String name;
@@ -45,3 +67,11 @@ class Dog extends Animal {
         dog.wagTail();
     }
 }
+
+/**
+ * abstraction is an important concept in object-oriented programming
+ * because it allows us to create higher-level, more general-purpose
+ * classes that can be used in a wide variety of situations, without having to
+ * worry about the specific details of each individual situation.
+ *
+ */
