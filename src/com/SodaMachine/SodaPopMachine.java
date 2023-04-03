22e28627dabaa9f298103a1416fa512e8f47c099
jcast6 <89822103+jcast6@users.noreply.github.com>
2023-04-03 11:38:14 -0700

    Added comments to understand concepts, such as encapsulation better. The userBuyingSoda() method was edited to have the user buy 1 or more sodas and remove the purchased amount and money from the vending machine. The correct messages for when a single drink or multiple drinks are purchased are displayed.
    The user can continue to make another purchase on top of the previous order now.

--- src/com/SodaMachine/SodaPopMachine.java
+++ src/com/SodaMachine/SodaPopMachine.java
@@ -1,22 +1,25 @@
 package com.SodaMachine;
 
-import javax.swing.plaf.synth.SynthTextAreaUI;
 import java.util.Scanner;
 
 public class SodaPopMachine {
+
+    // Private instance variables are used to encapsulate data
     private int numCans;
     private double canCost;
     private double money;
     private int amountBought;
 
+    // Constructor initializes the instance variables
     public SodaPopMachine(int numCans, double money, double canCost) {
         this.numCans = numCans;
         this.money = money;
         this.canCost = canCost;
     }
-
+/*
+    // This method simulates a customer buying a soda from the machine
     public void buySoda() {
-        if (numCans > 0) {
+        if (numCans > 1) {
             System.out.println("\nEnjoy your soda!");
             numCans--;
             amountBought++;
@@ -26,82 +29,85 @@ public class SodaPopMachine {
         }
     }
 
+
+ */
+    // This method adds cans to the machine
     public void refill(int numCansToAdd) {
         numCans += numCansToAdd;
         System.out.println("\nMachine refilled with " + numCansToAdd + " soda cans.");
     }
 
+    // This method displays the price of a soda
     public void getPrice() {
         System.out.println("\nPrice: " + canCost);
     }
 
+    // This method sets the price of a soda
     public void setCanCost(double canCost) {
         this.canCost = canCost;
     }
 
+    // This method sets the amount of sodas bought by the customer
     public void setAmountBought(int amountBought) {
 
         this.amountBought = amountBought;
     }
 
+    // This method displays the amount of sodas bought by the customer
     public void getAmountBought() {
         System.out.println("\nYou purchased " + amountBought + " sodas.");
     }
 
+    // This method displays the status of the vending machine
     public void displayStatus() {
         System.out.println("Number of soda cans: " + numCans);
         System.out.println("Amount of money in machine: $" + money);
         System.out.println("Price per soda: " + canCost);
     }
 
-    public void userBuyingSoda(int option) {
-        while(option != 0 && option != 1) {
-            System.out.println("Enter a valid option.");
+    // This method simulates a customer buying a soda from the machine
+    // It takes an integer option as input, which represents the customer's choice
+    public void userBuyingSoda(String option) {
+        while(!option.equals("exit")) {
+            if(option.equals("1")) {
+                System.out.println("Welcome to the soda vending machine.");
+                displayStatus();
+                System.out.println("Enter the number of sodas you want to buy:");
+                Scanner scanner = new Scanner(System.in);
+                int numSodasToBuy = scanner.nextInt();
+                if (numSodasToBuy <= numCans) {
+                    System.out.println("Thank you for your purchase!");
+                    numCans -= numSodasToBuy;
+                    amountBought += numSodasToBuy;
+                    money += numSodasToBuy * canCost;
+                    if (numSodasToBuy > 1) {
+                        System.out.println("\nEnjoy your drinks!");
+                        System.out.println("Would you like to purchase another drink. Enter 1 yes, 0 for no");
+                    } else {
+                        System.out.println("\nEnjoy your drink!");
+                        System.out.println("Would you like to purchase another drink.");
+                    }
+                } else {
+                    System.out.println("\nSorry, there are not enough sodas in the machine. Enter 1 yes, 0 for no");
+                }
+            } else if (option.equals("0")) {
+                System.out.println("Come back when you are thirsty!");
+            } else {
+                System.out.println("Enter a valid option or type 'exit' to quit.");
+            }
             Scanner newOption = new Scanner(System.in);
-            option = newOption.nextInt();
+            option = newOption.nextLine();
         }
-        if(option == 1) {
-            System.out.println("Welcome to the soda vending machine.");
-            displayStatus();
-        }
-        else if (option == 0) {
-            System.out.println("Come back when you are thirsty!");
-        }
-
     }
-/*
-    public static void main(String[] args) {
-        SodaPopMachine machine = new SodaPopMachine(10, 15.0, 1.5);
-        machine.displayStatus();
-        machine.buySoda();
-        machine.buySoda();
-
-        //Refill the sodas
-        machine.refill(3);
-        machine.refill(5);
-
-
-        // Show the amount of sodas bought
-        machine.getAmountBought();
-
-        //Buy soda and display soda machine properties
-        machine.buySoda();
-        machine.getAmountBought();
-        machine.displayStatus();
-
-    }
-}
-
- */
-
-     // Here we are trying to make the vending machine be used by user input
 
     public static void main(String[] args) {
         SodaPopMachine machine = new SodaPopMachine(10, 15.0, 1.5);
         Scanner userDecision = new Scanner(System.in);
-        System.out.println("Would you like to purchase a soda?");
-        int userOption = userDecision.nextInt();
+        System.out.println("Would you like to purchase a soda? Enter 1 for yes and 0 for no.");
+        String userOption = userDecision.next();
         machine.userBuyingSoda(userOption);
+
+
 
     }
 
